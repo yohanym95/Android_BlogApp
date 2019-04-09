b0504d5b6f8f35bfa34710778d9feb27534f1aac
yohanym95 <malshikay@gmail.com>
2019-04-09 08:42:38 +0530

    added navigation drawer with social media link and fixed bugs

--- app/src/main/java/com/example/yohan/blogapp/MainActivity.java
+++ app/src/main/java/com/example/yohan/blogapp/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.yohan.blogapp;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth mAuth;
     private Toolbar mToolbar;
     private ViewPager mViewPager;
     private SectionPagerAdapater sectionPagerAdapater;
     private TabLayout mTablLayout;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
 
 
     @Override
@@ -32,8 +42,25 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("ReadHub");
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerLayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(this );
+        navigationView.setItemIconTintList(null);
+
+
+
+
 
         mAuth = FirebaseAuth.getInstance();
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,mToolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         sectionPagerAdapater = new SectionPagerAdapater(getSupportFragmentManager());
 
@@ -75,6 +102,49 @@ public class MainActivity extends AppCompatActivity {
             FirebaseAuth.getInstance().signOut();;
             updateUI();
         }
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_facebook:
+               // Toast.makeText(getApplicationContext(),"Like us on Facebook",Toast.LENGTH_LONG).show();
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/readhublk"));
+                startActivity(i);
+                break;
+            case R.id.nav_lindin:
+               // Toast.makeText(getApplicationContext(),"Like us on Linkedin",Toast.LENGTH_LONG).show();
+                Intent i1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/company/readhub/"));
+                startActivity(i1);
+                break;
+            case R.id.nav_youtube:
+                //Toast.makeText(getApplicationContext(),"Like us on Youtube",Toast.LENGTH_LONG).show();
+                Intent i2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCbEkMN8pKtoV24vrhBrChhA"));
+                startActivity(i2);
+                break;
+            case R.id.nav_twiter:
+               // Toast.makeText(getApplicationContext(),"Like us on Youtube",Toast.LENGTH_LONG).show();
+                Intent i3 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/readhublk"));
+                startActivity(i3);
+                break;
+            case R.id.nav_contact:
+                Toast.makeText(getApplicationContext(),"Contact Us",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_about:
+                Toast.makeText(getApplicationContext(),"about us",Toast.LENGTH_LONG).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 }
